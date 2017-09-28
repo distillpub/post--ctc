559779a7e73264f5e78c99cd1e1a07b033053f68
Awni Hannun <awni.hannun@gmail.com>
2017-09-27 17:31:19 -0700

    v2 ctc collapse widget

--- assets/collapse_ctc.js
+++ assets/collapse_ctc.js
@@ -1,6 +1,11 @@
 var EPS = 'ϵ';
+var LOADED = false;
 
-$(document).ready(update())
+$(document).ready(function() {
+    update();
+    $("#alignment").blur();
+    LOADED = true;
+});
 
 $("#epsilon").click(function() {
     insert_epsilon();
@@ -12,6 +17,17 @@ $("#alignment").mousedown(function(e) {
     placeCaretAtEnd();
 });
 
+function first_focus() {
+    if (LOADED) {
+        var ali = $("#alignment");
+        ali.html("");
+        ali.off("focus", first_focus);
+    }
+    update();
+}
+
+$("#alignment").focus(first_focus);
+
 $("#alignment").keydown(function(e) {
     // Disable movement.
     if (e.keyCode >= 37 && e.keyCode <= 40) {
@@ -19,13 +35,13 @@ $("#alignment").keydown(function(e) {
     }
     // Handle holding backspace.
     if (e.keyCode == 8)
-        update(false);
+        update();
 });
 
 $("#alignment").keyup(function(e) {
     // Handle backspace.
     if (e.keyCode == 8)
-        update(false);
+        update();
 });
 
 $("#alignment").keypress(function(e) {
@@ -37,15 +53,16 @@ $("#alignment").keypress(function(e) {
         insert_epsilon();
     } else {
         ali_text = alignment.html();
-        alignment.html(ali_text + String.fromCharCode(e.keyCode));
+        ali_text += String.fromCharCode(e.keyCode);
+        alignment.html(ali_text);
     }
     update();
 });
 
-function update(animate) {
+function update() {
     var alignment = $("#alignment");
     placeCaretAtEnd();
-    update_output(animate);
+    update_output();
     var div = alignment.get()[0]
     alignment.scrollLeft(div.scrollWidth - div.clientWidth);
     check_empty()
@@ -62,75 +79,111 @@ function insert_epsilon() {
     eps_span = $("<span>");
     eps_span.html(EPS);
     eps_span.css("font-family","STIXGeneral-Italic");
+    eps_span.css("padding-left","3px");
+    eps_span.css("padding-right","3px");
     alignment.append(eps_span);
 }
 
-function update_output(anim) {
-    var  animate = (typeof anim !== 'undefined') ?  animate : true;
-
-    var out = $("#collapse_output");
-    var ali_text = $("#alignment").text();
-    var collapsed = ctc_collapse(ali_text);
-    out.html("");
-    for (var i = 0; i < collapsed.length - 1; i++)
-        out.append("<div>" + collapsed[i] + "</div>");
-
-    var ali_len = ali_text.length;
-    if (ali_len == 0)
-        return;
-
-    var last_char = "";
-    if (collapsed.length > 0) {
-        last_char = $("<div/>");
-        last_char.html(collapsed[collapsed.length-1]);
-    }
-        
-    if (ali_text[ali_len - 1] == EPS) {
-        // Add an epsilon
-        out.append(last_char);
-        var eps_div = $("<div/>");
-        eps_div.html(EPS);
-        eps_div.css("font-family", "STIXGeneral-Italic");
-
-        if (animate) {
-            eps_div.addClass("latest");
-            out.append(eps_div);
-            $(".latest").fadeTo(500, 0.2);
-        } else {
-            eps_div.css("opacity", "0.2");
-            out.append(eps_div)
-        }
-    } else if (ali_text.length > 1 && ali_text[ali_len -1] == ali_text[ali_len - 2]) {
-        // Add a repeat
-        out.append(last_char);
-        if (animate) { 
-            var repeat = $("<div/>");
-            repeat.html(ali_text[ali_len - 1]);
-            repeat.addClass("latest");
-            out.append(repeat);
-            $(".latest").fadeOut(500);
-        }
-    } else {
-        // Add a new character
-        if (animate) {
-            last_char.css("display", "none");
-            last_char.addClass("latest");
-            out.append(last_char);
-            $(".latest").fadeIn(500);
-        } else {
-            out.append(last_char);
-        }
-    }
+function add_rects(groups) {
+    return groups.append("rect")
+          .attr("width", 40)
+          .attr("height", 40)
+          .attr("fill", "#d3d3d3");
 }
 
-function ctc_collapse(alignment) {
-    var output = "";
+function add_text(groups) {
+    return groups.append("text")
+          .attr("x", 20)
+          .attr("y", 30)
+          .attr("text-anchor", "middle");
+}
+
+function update_alignment(alignment) {
+    var groups = d3.select("#align_g").selectAll("g");
+    groups = groups.data(alignment.split(""));
+    groups.exit().remove()
+    groups = groups.enter()
+          .append("g")
+          .attr("transform", function(d, i) {
+             return "translate(" + i * 42 + ",0)";
+           });
+    add_rects(groups);
+    add_text(groups).attr("class", function(d) {
+                  if (d == EPS)
+                      return "align_epsilon";
+                  else
+                      return "align_text";
+               })
+          .text(function(d) { return d;});
+}
+
+function update_merged(merged) {
+    groups = d3.select("#merge_g").selectAll("g");
+    groups = groups.data(merged);
+    groups.exit().remove();
+
+    groups.select("rect")
+          .attr("width", function(d) { return (d["end"] - d["start"]) * 42 - 2; });
+
+    groups = groups.enter()
+          .append("g")
+          .attr("transform", function(d) {
+              return "translate(" + d["start"] * 42 + ",0)";
+          })
+          .attr("opacity", function(d) {
+              var t = d["char"];
+              if (t == EPS) return 0.6;
+              return 1;
+          });
+    rects = add_rects(groups);
+    rects.attr("width", function(d) { return (d["end"] - d["start"]) * 42 - 2; });
+    add_text(groups).attr("class", "align_text")
+          .text(function(d) {
+              var t = d["char"];
+              if (t == EPS) return "";
+              return t;
+          });
+} 
+
+function update_final(collapsed) {
+    var groups = d3.select("#final_g").selectAll("g");
+    groups = groups.data(collapsed)
+    groups.exit().remove()
+    groups = groups.enter()
+          .append("g")
+          .attr("transform", function(d, i) {
+              return "translate(" + i * 42 + ",0)";
+          })
+    add_rects(groups);
+    text = add_text(groups).attr("class", "align_text")
+          .text(function(d) { return d; });
+}
+
+function update_output() {
+    var alignment = $("#alignment").text();
+    update_alignment(alignment);
+
+    merged = merge(alignment);
+    update_merged(merged);
+
+    // The final output.
+    collapsed = []
+    for (var i = 0; i < merged.length; i++) {
+        var c = merged[i]["char"];
+        if (c != EPS)
+            collapsed.push(c);
+    }
+    update_final(collapsed);
+}
+
+function merge(alignment) {
+    var output = [];
     for (var i = 0; i < alignment.length; i++) {
-        if (alignment[i] == EPS)
-            continue;
-        if (i != 0 && alignment[i] == alignment[i-1])
-            continue;
-        output += alignment[i];
+        if (i != 0 && alignment[i] == alignment[i-1]) {
+            output[output.length - 1]["end"] = i + 1;
+        } else {
+            output.push({"char" : alignment[i], "start": i, "end" : i + 1});
+        }
     }
     return output;
 }
